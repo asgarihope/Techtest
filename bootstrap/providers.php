98351c8b27b426746465f4609be073486e042d2e
Omid <omidasgari.rade@gmail.com>
2024-07-30 13:52:19 +0330

    [FEATURE] bootstrap

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\InterfaceRepositoryServiceProvider::class,
+    App\Providers\InterfaceServiceServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    Laravel\Passport\PassportServiceProvider::class,
+];
